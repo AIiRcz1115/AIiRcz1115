763f7ead3f7b6b7a4b109f50ee7fcdc44eb3d4e5
Kinoll <kinoll94@gmail.com>
2016-03-16 00:26:08 +0100

    create-devenv.sh updated

--- server/create-devenv.sh
+++ server/create-devenv.sh
@@ -5,8 +5,8 @@ sudo apt-get update
 
 sudo apt-get install python3-pip
 sudo pip3 install virtualenv
-virtualenv venv
-source venv/bin/activate
+virtualenv server/venv
+source server/venv/bin/activate
 pip3 install django
 django-admin --version
 deactivate
